751daae15297029a45154bbbc3ae0850d265a572
SoundAsleep192 <52359798+SoundAsleep192@users.noreply.github.com>
2020-05-16 19:50:39 +0300

    fill database with data

--- database.sql
+++ database.sql
@@ -1,7 +1,16 @@
 CREATE DATABASE covidstats;
 
-CREATE TABLE countries(
-    country_id SERIAL PRIMARY KEY,
+CREATE TABLE covid_global(
+    day DATE,
     country_code VARCHAR(2),
-    country_name VARCHAR(56)
+    country_name VARCHAR(56),
+    region VARCHAR(5),
+    deaths INTEGER,
+    cumulative_deaths INTEGER,
+    confirmed_cases INTEGER,
+    cumulative_confirmed_cases INTEGER
 );
+
+COPY covid_global FROM '*absolute_path*\WHO-COVID-19-global-data.csv' DELIMITER ',' CSV HEADER;
+
+SELECT MIN(day) FROM covid_global;
